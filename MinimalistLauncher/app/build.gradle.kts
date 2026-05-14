2e9cdba064aefbb608566fbe85490977d317c461
rishi <rishi7258prince@gmail.com>
2026-05-15 04:21:32 +0530

    feat: Build the app and installed in phone

--- MinimalistLauncher/app/build.gradle.kts
+++ MinimalistLauncher/app/build.gradle.kts
@@ -53,5 +53,6 @@ dependencies {
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.biometric)
+    implementation(libs.google.material)
     debugImplementation(libs.androidx.ui.tooling)
 }
